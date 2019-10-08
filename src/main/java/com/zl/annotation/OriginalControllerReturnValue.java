4da7814b7133bc06c896124f5ac24ed5e81b3d43
zhang liang <906356464@qq.com>
2019-10-08 11:19:08 +0800

    mybatis

--- src/main/java/com/zl/annotation/OriginalControllerReturnValue.java
+++ src/main/java/com/zl/annotation/OriginalControllerReturnValue.java
@@ -4,9 +4,9 @@ import java.lang.annotation.*;
 
 /**
  *不通过统一的响应处理，可以使用此注解
+ * @author tzxx
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface OriginalControllerReturnValue {
-}
+public @interface OriginalControllerReturnValue {}
