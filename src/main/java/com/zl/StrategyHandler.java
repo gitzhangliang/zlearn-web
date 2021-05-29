9e1c98693471ff08b763ca5dc2ee6d10a25fea40
zhang liang <906356464@qq.com>
2021-05-30 00:16:06 +0800

    tree

---
+++ src/main/java/com/zl/StrategyHandler.java
@@ -0,0 +1,19 @@
+package com.zl;
+
+/**
+ * @author
+ * @date
+ */
+public interface StrategyHandler<T, R> {
+
+    @SuppressWarnings("rawtypes")
+    StrategyHandler DEFAULT = t -> null;
+
+    /**
+     * apply strategy
+     *
+     * @param param
+     * @return
+     */
+    R apply(T param);
+}
